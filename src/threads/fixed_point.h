ab24e7065431d332b4a13ab7fae4d5ae6c9295aa
Ying-Yu Ho <heinsolid@gmail.com>
2014-02-05 17:26:08 -0800

    disabled conversion from fixed to float point
    float point is not supported
    the test file no longer works

--- src/threads/fixed_point.h
+++ src/threads/fixed_point.h
@@ -11,9 +11,25 @@ typedef int32_t fp_t;
 // 2 ** FP_Q
 #define FP_F (1 << FP_Q)
 
+fp_t fp_from_int(int32_t n);
+
+int32_t fp_floor(fp_t x);
+int32_t fp_round(fp_t x);
+
+fp_t fp_add_fp(fp_t x, fp_t y);
+fp_t fp_sub_fp(fp_t x, fp_t y);
+fp_t fp_mul_fp(fp_t x, fp_t y);
+fp_t fp_div_fp(fp_t x, fp_t y);
+
+fp_t fp_add_int(fp_t x, int32_t n);
+fp_t fp_sub_int(fp_t x, int32_t n);
+fp_t fp_mul_int(fp_t x, int32_t n);
+fp_t fp_div_int(fp_t x, int32_t n);
+
+
 fp_t fp_from_int(int32_t n) { return n << FP_Q; }
 
-float fp_to_float(fp_t x) { return 1.0 * x / FP_F; }
+// float fp_to_float(fp_t x) { return 1.0 * x / FP_F; }
 
 int32_t fp_floor(fp_t x) { return x / FP_F; }
 
