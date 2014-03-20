69bdc81e3a9912bcf5ab7c2ddb59c4e54c90ec89
Ying-Yu Ho <heinsolid@gmail.com>
2014-03-19 23:16:52 -0700

    very buggy buffer cache
    cannot pass any test yet. panicked in cache.c:get_cache() at hash_find.
    didn't really know why so it is not used in commited version yet

--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -4,10 +4,13 @@
 #include <stdint.h>
 
 #include "devices/block.h"
+#include "filesys/off_t.h"
 
 #define CACHE_SECTORS 64
 
 void cache_init (void);
 
+void cache_read (block_sector_t idx, off_t ofs, uint8_t *dest, size_t size);
+void cache_write (block_sector_t idx, off_t ofs, const uint8_t *src, size_t size);
 
 #endif /* filesys/cache.h */
